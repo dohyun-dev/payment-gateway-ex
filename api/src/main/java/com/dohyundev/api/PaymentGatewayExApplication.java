b47ec169e6513723a367242b5737421c693e3294
dohyundev0425 <kwondh0351@modaoutlet.co.kr>
2025-06-24 10:50:16 +0900

    project init

---
+++ api/src/main/java/com/dohyundev/api/PaymentGatewayExApplication.java
@@ -0,0 +1,13 @@
+package com.dohyundev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentGatewayExApplication.class, args);
+    }
+
+}
